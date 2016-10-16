ded18f75d3221b759402b7a0a997de6eb68c1d95
rus1147 <rus1147@gmail.com>
2016-10-16 20:47:27 -0300

    main original

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,10 @@
-#include <fstream>
 #include "../include/tipos.h"
 #include "../include/atleta.h"
 #include "../include/competencia.h"
 #include "../include/jjoo.h"
-#include "../include/auxiliares.h"
 
 
 int main() {
-     /* Atleta a1("Carlos",Masculino,1982,"Argentina",32);
-      Atleta a2("Maria",Femenino,1994,"Rusia",13);
-      a1.mostrar(cout);
-      ofstream guardarAtleta("guardarAtleta.txt");
-      a1.guardar(guardarAtleta);
-      ifstream cargarAtleta("guardarAtleta.txt");
 
-      a2.mostrar(cout);
-      ofstream guardarAtleta2("guardarAtleta2.txt");
-      a2.guardar(guardarAtleta2);
-      ifstream cargarAtleta2("guardarAtleta2.txt");*/
-      return 0;
+    return 0;
 }
