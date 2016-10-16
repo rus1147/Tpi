9bfd5f2f202dde38f2e452e85223e714c700615b
rus1147 <rus1147@gmail.com>
2016-10-16 18:55:16 -0300

    Demostracion AtletaProdigio sin terminar

---
+++ src/ojota.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "../include/atleta.h"
+#include "../include/jjoo.h"
+
+using namespace std;
+Atleta atletaProdigio(const JJOO &j){
+    Competencia CompetenciaFin;
+    Atleta a;
+    bool;
+    int i=0;
+    while(i<CompetenciaFin.size()){
+        if(!CompetenciaFin.finalizada()){
+
+        }
+    }
+}
