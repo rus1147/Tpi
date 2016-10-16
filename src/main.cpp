d8ba6ddf6d1afd940ada84edee5eb37914d1cdea
rus1147 <rus1147@gmail.com>
2016-10-16 17:48:13 -0300

    rush b

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,16 @@
 
 
 int main() {
+     /* Atleta a1("Carlos",Masculino,1982,"Argentina",32);
+      Atleta a2("Maria",Femenino,1994,"Rusia",13);
+      a1.mostrar(cout);
+      ofstream guardarAtleta("guardarAtleta.txt");
+      a1.guardar(guardarAtleta);
+      ifstream cargarAtleta("guardarAtleta.txt");
+
+      a2.mostrar(cout);
+      ofstream guardarAtleta2("guardarAtleta2.txt");
+      a2.guardar(guardarAtleta2);
+      ifstream cargarAtleta2("guardarAtleta2.txt");*/
       return 0;
 }
