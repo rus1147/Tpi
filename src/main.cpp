b18c069b24a019eb042970ce006f8c39f7616ca4
rus1147 <rus1147@gmail.com>
2016-10-16 03:00:26 -0300

    Agrego un test que tengo que corregir ya que no se puede correr

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
+#include <fstream>
 #include "../include/tipos.h"
 #include "../include/atleta.h"
 #include "../include/competencia.h"
 #include "../include/jjoo.h"
+#include "../include/auxiliares.h"
 
 int main() {
-
+  /*  ifstream entrada("entrada.txt");
+    ofstream salida("salida.txt");
+    int cant,i=0;
+    entrada >> cant;
+    while (i<cant){
+        Atleta Atleta;
+        atleta.cargar(entrada);
+        atleta.mostrar(cout);
+        atleta.guardar(salida);
+        i++;
+    }
+    entrada.close();
+    salida.close(); */
+    return 0;
 }
---
+++ tests/test cargar y mostrar atleta.cpp
@@ -0,0 +1,46 @@
+#include <fstream>
+#include <algorithm>
+#include <atleta.h>
+#include "gtest/gtest.h"
+#include "auxiliares_tests.h"
+
+/*
+TEST(atleta_nuevo_deporte, guardar_y_cargar_funcionan_ok) {
+//|Liu Song| |Masculino| 1972 |China| 123 [(|Tenis de Mesa|, 90)]
+
+Atleta a1("Nole", Genero::Masculino, 1983, "Serbia", 5);
+atleta.entrenarNuevoDeporte(deportes[0], 7);
+
+char fname[] = "test_atleta.txt";
+std::ofstream out;
+out.open(fname);
+a1.guardar(out);
+Atleta a2;
+out.close();
+std::ifstream in(fname);
+a2.cargar(in);
+in.close();
+
+EXPECT_EQ(a2, a1);
+}
+
+TEST(test_drone_lo_basico, guardar_y_cargar_trayectoria_funciona_ok) {
+Drone d1(1, algunos_productos());
+
+char fname[] = "test_drone.txt";
+std::ofstream out;
+out.open(fname);
+d1.moverA({1, 3});
+d1.moverA({2, 3});
+d1.moverA({3, 3});
+d1.guardar(out);
+Drone d2;
+out.close();
+std::ifstream in(fname);
+d2.cargar(in);
+in.close();
+
+EXPECT_EQ(d2, d1);
+}
+
+ * /
