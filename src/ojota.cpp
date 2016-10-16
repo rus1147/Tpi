a53cfa02fa4e86ac09ccd19cbff0234049bddc3f
rus1147 <rus1147@gmail.com>
2016-10-16 20:22:38 -0300

    Demostracion AtletaProdigio sin terminarv1

--- src/ojota.cpp
+++ src/ojota.cpp
@@ -3,14 +3,36 @@
 #include "../include/jjoo.h"
 
 using namespace std;
+vector<Competencia> Competenciaconoroenpodio(JJOO const &j) {
+    vector<Atleta> Ganadores;
+    int i = 0;
+    while (i < Competenciaconoroenpodio(j).size()) {
+        Ganadores.push_back(Competenciaconoroenpodio.ranking()[0]);
+        i++;
+    }
+    int k = 0;
+    while (k < Ganadores.size(j)) {
+        int t = Ganadores[0].anioNacimiento();
+        if (t < Ganadores[j].anioNacimiento()) {
+            t = Ganadores;
+        }
+        k++;
+    }
+
+    return t;
+}
+//ese es la de willy
+//sino
+/*
 Atleta atletaProdigio(const JJOO &j){
     Competencia CompetenciaFin;
     Atleta a;
-    bool;
+    bool esCampeon=false;
     int i=0;
     while(i<CompetenciaFin.size()){
         if(!CompetenciaFin.finalizada()){
-
+            i++;
         }
+        else
     }
-}
+}*/
